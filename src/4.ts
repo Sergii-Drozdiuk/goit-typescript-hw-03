53e60378b56366d7e4e047dc7818336ce57cb314
Sergii-Drozdiuk <drozduyk@gmail.com>
2023-12-01 00:17:19 +0200

    HW-3

--- src/4.ts
+++ src/4.ts
@@ -1,10 +1,59 @@
-const key = new Key();
+class Key {
+   private signature: number;
 
+   constructor() {
+      this.signature = Math.random();
+   }
+
+   getSignature(): number {
+      return this.signature;
+   }
+}
+
+class Person {
+
+   constructor(private key: Key) {
+      this.key = key;
+   }
+
+   getKey() {
+      return this.key;
+   }
+}
+
+abstract class House {
+   protected door: boolean;
+   protected key: Key;
+   protected tenants: Person[];
+
+   constructor(key: Key) {
+      this.door = false;
+      this.key = key;
+      this.tenants = [];
+   }
+
+   abstract openDoor(key: Key): void;
+
+   comeIn(person: Person): void {
+      if (this.door) {
+         this.tenants.push(person);
+      }
+   }
+}
+
+class MyHouse extends House {
+   openDoor(key: Key): void {
+      if (key.getSignature() === this.key.getSignature()) {
+         this.door = true;
+      }
+   }
+}
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
-
 house.comeIn(person);
 
 
